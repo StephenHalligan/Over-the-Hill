006d586cf903d1ab2b11942e35b7dfd418936aa0
Stephen Halligan <sldhalligan@gmail.com>
2022-03-29 20:07:11 +0300

    Worked on profile activity

--- Project/app/src/main/java/com/example/overthehill/ProfileActivity.java
+++ Project/app/src/main/java/com/example/overthehill/ProfileActivity.java
@@ -3,6 +3,9 @@ package com.example.overthehill;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +25,9 @@ import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private Button mSaveChanges;
+    private EditText mName, mEmail, mLocation, mAge, mBio, mInterests;
+
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
@@ -29,5 +35,52 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            }
+        };
+
+        mSaveChanges = (Button) findViewById(R.id.SaveChanges);
+
+        mName = (EditText) findViewById(R.id.Name);
+        mEmail = (EditText) findViewById(R.id.Email);
+        mLocation = (EditText) findViewById(R.id.Location);
+        mAge = (EditText) findViewById(R.id.Age);
+        mBio = (EditText) findViewById(R.id.Bio);
+        mInterests = (EditText) findViewById(R.id.Interests);
+
+        mSaveChanges.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                final String name = mName.getText().toString();
+                final String email = mEmail.getText().toString();
+                final String location = mLocation.getText().toString();
+                final String age = mAge.getText().toString();
+                final String bio = mBio.getText().toString();
+                final String interests = mInterests.getText().toString();
+
+                String userId = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+                DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Name");
+                currentUserDb.setValue(name);
+                currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Email");
+                currentUserDb.setValue(email);
+                currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Location");
+                currentUserDb.setValue(location);
+                currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Age");
+                currentUserDb.setValue(age);
+                currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Bio");
+                currentUserDb.setValue(bio);
+                currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Interests");
+                currentUserDb.setValue(interests);
+
+            }
+        });
+
     }
 }
